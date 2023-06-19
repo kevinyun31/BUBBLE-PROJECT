98b1a74b0beb3708f64b7ca91cc0a35907435797
rkawkclsrn <rkawkclsrn@naver.com>
2023-06-19 17:52:57 +0900

    BUBBLE 23.06.19

---
+++ src/test/ex032/Player.java
@@ -0,0 +1,48 @@
+package test.ex032;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Player extends JLabel {
+
+    private int x;
+    private int y;
+
+    private ImageIcon playerR, playerL;
+
+    public Player() {
+        initObject();
+        initSetting();
+    }
+
+    private void initObject() {
+        playerR = new ImageIcon("image/playerR.png");
+        playerL = new ImageIcon("image/playerL.png");
+    }
+
+    private void initSetting() {
+        x = 70;
+        y = 535;
+
+        setIcon(playerR);
+        setSize(50, 50);
+        setLocation(x, y);
+    }
+
+    public void right() {
+        setIcon(playerR);
+        while (true) {
+            x = x + 10;
+            setLocation(x, y);
+
+            Thread.sleep(10);
+        }
+    }
+
+    public void left() {
+        setIcon(playerL);
+        x = x - 10;
+        setLocation(x, y);
+    }
+
+}
